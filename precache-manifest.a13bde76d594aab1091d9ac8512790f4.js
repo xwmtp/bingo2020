6740c113248b2969a495840b49b00ec363740646
xwmtp <xwillmtp@gmail.com>
2020-07-20 17:10:45 +0200

    Updates

---
+++ precache-manifest.a13bde76d594aab1091d9ac8512790f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e258b63e710c971d857c4105ef890d",
+    "url": "/bingo2020/index.html"
+  },
+  {
+    "revision": "8535a1aa2d7127763b9d",
+    "url": "/bingo2020/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "22f33b5b30383b482b6d",
+    "url": "/bingo2020/static/js/2.052bf55d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bingo2020/static/js/2.052bf55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8535a1aa2d7127763b9d",
+    "url": "/bingo2020/static/js/main.08154b8b.chunk.js"
+  },
+  {
+    "revision": "267370ada0d4bfe79eb4",
+    "url": "/bingo2020/static/js/runtime-main.8e2ca924.js"
+  }
+]);
