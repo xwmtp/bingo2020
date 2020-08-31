8b5d79d95cfcc8000f7670d11a3a9611eb5057a6
xwmtp <xwillmtp@gmail.com>
2020-08-31 21:36:18 +0200

    Updates

--- precache-manifest.7e7de18535971c7f146fa65f8a205993.js
+++ precache-manifest.7e7de18535971c7f146fa65f8a205993.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4400538ab6aa24f40da1f38290115a1a",
+    "revision": "b66c1f2b1a754f061169e5382a06d80d",
     "url": "/bingo2020/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bingo2020/static/js/2.8105420d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1e61cf65f84e47c97aa9",
-    "url": "/bingo2020/static/js/main.2b113d08.chunk.js"
+    "revision": "f6f85a2886624b71f762",
+    "url": "/bingo2020/static/js/main.291db3c6.chunk.js"
   },
   {
     "revision": "267370ada0d4bfe79eb4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bingo2020/precache-manifest.61af854b8bcb0fb0fb734f680c6d7ae2.js"
+  "/bingo2020/precache-manifest.7e7de18535971c7f146fa65f8a205993.js"
 );
 
 self.addEventListener('message', (event) => {
